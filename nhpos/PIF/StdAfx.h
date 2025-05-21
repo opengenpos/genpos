8bdc6685c587291e22bd154e30ed1141c08a3459
RichardChambers <rickchambers.geo@gmail.com>
2025-05-21 14:05:50 -0400

    update the StdAfx.h file with the versioninfo.h and other standard stuff that has been added to the other StdAfx.h files in order to standardize the target Windows version.

--- nhpos/PIF/StdAfx.h
+++ nhpos/PIF/StdAfx.h
@@ -12,6 +12,36 @@
 
 
 // Insert your headers here
+
+#include "versioninfo.h"
+
+// Modify the following defines if you have to target a platform prior to the ones specified below.
+// Refer to MSDN for the latest info on corresponding values for different platforms.
+// See Using the Windows Headers  https://msdn.microsoft.com/library/windows/desktop/aa383745
+// See Modifying WINVER and _WIN32_WINNT https://msdn.microsoft.com/en-us/library/6sehtctf.aspx
+//    Windows XP       0x0501
+//    Windows XP SP2   0x0502
+//    Windows 7        0x0601
+//    Windows 8        0x0602
+//    Windows 8.1      0x0603
+//    Windows 10       0x0A00
+
+#if !defined(WINVER_FOR_GENPOS)
+#define WINVER_FOR_GENPOS  0x0601          // GenPOS targets Windows 7 and Windows POS Ready 7 and later.
+#endif
+
+#ifndef WINVER                             // Allow use of features specific to Windows XP or later.
+#define WINVER WINVER_FOR_GENPOS           // Change this to the appropriate value to target other versions of Windows.
+#endif
+
+#ifndef _WIN32_WINNT                       // Allow use of features specific to Windows XP or later.                   
+#define _WIN32_WINNT WINVER_FOR_GENPOS     // Change this to the appropriate value to target other versions of Windows.
+#endif						
+
+#ifndef _WIN32_WINDOWS                     // Allow use of features specific to Windows 98 or later.
+#define _WIN32_WINDOWS WINVER_FOR_GENPOS   // Change this to the appropriate value to target other versions of Windows.
+#endif
+
 #define WIN32_LEAN_AND_MEAN		// Exclude rarely-used stuff from Windows headers
 
 #include <windows.h>
